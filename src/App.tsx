400724f2d3477c9392b459c0d87b79a03651a188
gky360 <gky360@gmail.com>
2020-11-09 00:33:13 +0900

    Install react-chartjs-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { ReactChartjs2 } from './ReactChartjs2';
 import { Recharts } from './Recharts';
 
 const ROUTES = {
+  HOME: '/',
   RECHARTS: '/recharts',
+  REACT_CHARTJS_2: '/react-chartjs-2',
 };
 
 function App() {
@@ -15,10 +18,13 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to={ROUTES.HOME}>Home</Link>
             </li>
             <li>
               <Link to={ROUTES.RECHARTS}>recharts</Link>
+            </li>
+            <li>
+              <Link to={ROUTES.REACT_CHARTJS_2}>react-chartjs-2</Link>
             </li>
           </ul>
         </nav>
@@ -26,6 +32,9 @@ function App() {
         <Switch>
           <Route path={ROUTES.RECHARTS}>
             <Recharts />
+          </Route>
+          <Route path={ROUTES.REACT_CHARTJS_2}>
+            <ReactChartjs2 />
           </Route>
         </Switch>
       </div>
